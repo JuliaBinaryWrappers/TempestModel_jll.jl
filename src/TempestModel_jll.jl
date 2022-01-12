1844e50b707d8d78389c2a7d28c92e27fb6138d1
jlbuild <juliabuildbot@gmail.com>
2022-01-12 18:16:35 +0000

    TempestModel_jll build 0.1.0+0

---
+++ src/TempestModel_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule TempestModel_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("TempestModel")
+JLLWrappers.@generate_main_file("TempestModel", UUID("2962c967-6e2c-5a30-9da7-7ff16ffb0b2a"))
+end  # module TempestModel_jll
